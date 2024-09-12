8fbd3c1d5ddcf25431c4d199abc6165bb98c345b
danikkkkk12 <danilmalyk20@gmail.com>
2024-09-12 18:00:08 +0300

    +

---
+++ index.js
@@ -0,0 +1,36 @@
+let age = 15;
+console.log(age);
+
+let myName = "danylo";
+console.log(myName);
+
+
+let isStudent = confirm("Student?");
+console.log(isStudent);
+
+let myString = ("Найздоровіша відповідь на життя – це радість");
+console.log(myString);
+
+let myNumber = 15;
+myNumber = myNumber + 10;
+console.log(myNumber);
+
+let myNull = "null";
+console.log(myNull);
+
+let userName = promt("Введіть своє ім'я:");
+
+alert("Привіт, " + userName);
+
+let isConfirmed = confirm("Ви підтверджуєте дію?");
+
+alert(isConfirmed ? "Дякую за підтвердження!" : "Дію відмінено!")
+
+alert("Увага! Ця дія небезпечна.");
+
+let isConfirme = confirm("Ви підтверджуєте дію?");
+
+alert(isConfirme ? "Дякую за підтвердження!" : "Дію відмінено!")
+
+
+
